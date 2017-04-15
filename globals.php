c89c5c4be24b94032a32174b8bf3aaaa923fb1ca
Kevin Trotter <cskevint@gmail.com>
2017-04-15 08:14:50 -0700

    updated meta content

--- globals.php
+++ globals.php
@@ -3,18 +3,19 @@
 $website_url = "http://sanmateo.junioryouth.us";
 $website_title = "San Mateo Junior Youth Spiritual Empowerment Program";
 $website_short_title = "San Mateo JYSEP";
-$website_short_description = "Inspiring young people, parents, and community members to work together toward a better world.";
-$website_meta_description = "A global volunteer and mentoring program inspiring young people, parents, and community members to work together toward a better world. Over the past 10 years, the program has grown throughout Chapel Hill, Carrboro, Durham, Cary, Raleigh.";
-$website_meta_keywords = "Triangle, Junior Youth Spiritual Empowerment Program, Chapel Hill, Carrboro, Durham, Cary, Raleigh, Volunteer, Mentor, Middle School, Youth, Service, After School Program, Community, Spiritual, Religion, Social Action, Chapelboro";
-$website_about = "Serving the region for more than 10 years, the Junior Youth Spiritual Empowerment Program is a mentoring initiative that inspires young people, parents, and community members to work together toward a better world.";
-
-$facebook_page = "https://www.facebook.com/sanmateojysep";
 
 $program_address = "30 N Grant St<br>San Mateo, CA 94401";
 $program_email = "sanmateojysep@gmail.com";
 $program_phone = "(650) 999-0019";
 $program_state = "California";
 $program_location = "San Mateo County";
+
+$website_short_description = "Inspiring young people, parents, and community members to work together toward a better world.";
+$website_meta_description = "A global volunteer and mentoring program inspiring young people, parents, and community members to work together toward a better world. Over the past 10 years, the program has grown throughout ".$program_location;
+$website_meta_keywords = "Junior Youth Spiritual Empowerment Program, Volunteer, Mentor, Middle School, Youth, Service, After School Program, Community, Spiritual, Religion, Social Action";
+$website_about = "Serving the region for more than 10 years, the Junior Youth Spiritual Empowerment Program is a mentoring initiative that inspires young people, parents, and community members to work together toward a better world.";
+
+$facebook_page = "https://www.facebook.com/sanmateojysep";
 
 $contact_from_email = "core@sanmateo.junioryouth.us";
 
